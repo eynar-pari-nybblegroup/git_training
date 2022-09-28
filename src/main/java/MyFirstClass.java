708bc42afe2a4844db12b9a85d64d836282142b9
eynar-pari-nybblegroup <eynar.pari@nybblegroup.com>
2022-09-28 09:20:43 -0400

    ADD > eynar branch

--- src/main/java/MyFirstClass.java
+++ src/main/java/MyFirstClass.java
@@ -1,2 +1,11 @@
 public class MyFirstClass {
+
+    public MyFirstClass(){
+        System.out.println("Eynar2222");
+    }
+
+    public void helloWorld(){
+        System.out.println("Hola Eynar2222");
+    }
+
 }
