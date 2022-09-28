73487f83b5475ed5cb0bf9c4fb595f05ebf48654
rmnhernan <60717000+rmnhernan@users.noreply.github.com>
2022-09-28 10:20:58 -0300

    UPDATE > cambios branch fedeRoman

--- src/main/java/MyFirstClass.java
+++ src/main/java/MyFirstClass.java
@@ -1,2 +1,11 @@
-public class MyFirstClass {
+public class MyFirstClass
+{
+    public MyFirstClass()
+    {
+        System.out.println("Fede Roman");
+    }
+    public void helloWord()
+    {
+        System.out.println("Hello Fede Roman");
+    }
 }
