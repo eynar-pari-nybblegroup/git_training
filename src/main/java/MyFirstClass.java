cb352505c94f3639af975b241475c9ec1bceb1b4
fedenybble <federico.padin@nybblegroup.com>
2022-09-28 10:27:12 -0300

    Cambio Fede Padin

--- src/main/java/MyFirstClass.java
+++ src/main/java/MyFirstClass.java
@@ -1,2 +1,12 @@
 public class MyFirstClass {
+
+    public MyFirstClass(){
+        System.out.println("Fede Padin");
+    }
+
+    public void helloWorld(){
+        System.out.println("Hola Fede Padin");
+    }
+
 }
+
